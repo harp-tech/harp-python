6daba190d1ddd85c054d1f670add5a1e262852c1
glopesdev <glopesdev@users.noreply.github.com>
2023-10-30 12:45:46 +0000

    Add base implementation of register reader

---
+++ harp/reader.py
@@ -0,0 +1,71 @@
+import re
+from functools import partial, reduce
+from pandas import DataFrame
+from typing import Iterable, Callable
+from harp.model import Model, Register
+from harp.io import read
+
+_camel_to_snake_regex = re.compile(r"(?<!^)(?=[A-Z])")
+
+
+class Reader:
+    register: Register
+    read: Callable[[str], DataFrame]
+
+    def __init__(self, register: Register, read: Callable[[str], DataFrame]) -> None:
+        self.register = register
+        self.read = read
+
+
+def compose(f, g):
+    return lambda *a, **kw: f(g(*a, **kw))
+
+
+def id_camel_to_snake(id: str):
+    return _camel_to_snake_regex.sub("_", id).lower()
+
+
+def keys_camel_to_snake(keys: Iterable[str]):
+    return [id_camel_to_snake(k) for k in keys]
+
+
+def create_bitreader(mask):
+    return lambda xs: ((xs & mask) != 0)
+
+
+def create_reader(device: Model, name: str):
+    register = device.registers[name]
+    reader = read
+
+    if register.maskType is not None:
+        key = register.maskType.root
+        bitMask = device.bitMasks.get(key)
+        if bitMask is not None:
+            lookup = [
+                (id_camel_to_snake(k), create_bitreader(v.root))
+                for k, v in bitMask.bits.items()
+            ]
+
+            def unpack(df):
+                return DataFrame({n: f(df[0]) for n, f in lookup}, index=df.index)
+
+            reader = compose(unpack, reader)
+            return Reader(register, reader)
+
+        groupMask = device.groupMasks.get(key)
+        if groupMask is not None:
+            name = id_camel_to_snake(name)
+            lookup = {value.root: name for name, value in groupMask.values.items()}
+            reader = partial(reader, columns=[name])
+            reader = compose(lambda df: df.map(lambda x: lookup[x]), reader)
+            return Reader(register, reader)
+
+    if register.payloadSpec is not None:
+        columns = register.payloadSpec.keys()
+        columns = keys_camel_to_snake(columns)
+        reader = partial(reader, columns=columns)
+        return Reader(register, reader)
+
+    columns = [id_camel_to_snake(name)]
+    reader = partial(reader, columns=columns)
+    return Reader(register, reader)
