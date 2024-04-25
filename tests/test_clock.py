e6058d32e33b2de9e78d61e8bbdc54585263075e
Josh Siegle <joshs@alleninstitute.org>
2024-04-25 16:38:54 -0700

    Add support for timestamps in seconds

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -17,6 +17,7 @@ testinput = [
             1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
             1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
             1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]),
+        'sample_rate': 30000.,
         'expected_start_samples': np.array([28832, 58835]),
         'expected_harp_times': np.array([3806874, 3806875])
     },
@@ -30,7 +31,26 @@ testinput = [
         'states': np.array([
             1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
             1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]),
+        'sample_rate': 30000.,
         'expected_start_samples': np.array([28833, 58835]),
+        'expected_harp_times': np.array([2600960, 2600961])
+    },
+    {
+        'sample_numbers': np.array([
+            0.        , 0.9611    , 0.9701    , 0.9711    , 0.9761    ,
+            0.9781    , 0.9791    , 0.9811    , 0.9821    , 0.9851    ,
+            0.9871    , 0.9881    , 0.9901    , 0.9911    , 1.0001    ,
+            1.96116667, 1.96216667, 1.96316667, 1.97016667, 1.97116667,
+            1.97616667, 1.97816667, 1.97916667, 1.98116667, 1.98216667,
+            1.98516667, 1.98716667, 1.98816667, 1.99016667, 1.99116667,
+            1.99616667, 1.99816667, 1.99916667, 2.0012    , 2.00216667,
+            2.0052    , 2.0072    , 2.0082    , 2.0102    , 2.0112    ,
+            2.0202    , 2.96526667]),
+        'states': np.array([
+            1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+            1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]),
+        'sample_rate' : 1,
+        'expected_start_samples': np.array([0.9611, 1.96116667]),
         'expected_harp_times': np.array([2600960, 2600961])
     }
 ]
@@ -40,7 +60,10 @@ testinput = [
 @mark.parametrize("test_input", testinput)
 def test_create_reader(test_input):
     start_samples, harp_times = decode_harp_clock(
-        test_input["sample_numbers"], test_input["states"]
+        test_input["sample_numbers"],
+        test_input["states"],
+        sample_rate=test_input["sample_rate"],
+        baud_rate=1000,
     )
 
     assert np.allclose(start_samples, test_input["expected_start_samples"])
