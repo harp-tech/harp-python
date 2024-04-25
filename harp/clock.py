f6cc65b0c9d6cc13cea5b73c968ded0261e49618
Josh Siegle <joshs@alleninstitute.org>
2024-04-25 16:38:54 -0700

    Add support for timestamps in seconds

--- harp/clock.py
+++ harp/clock.py
@@ -2,7 +2,9 @@ import numpy as np
 import warnings
 
 
-def decode_harp_clock(sample_numbers, states, sample_rate=30000.0, baud_rate=1000.0):
+def decode_harp_clock(
+    timestamps_or_sample_numbers, states, sample_rate=1, baud_rate=1000.0
+):
     """
     Decodes Harp clock times (in seconds) from a sequence of sample
     numbers and states.
@@ -32,34 +34,35 @@ def decode_harp_clock(sample_numbers, states, sample_rate=30000.0, baud_rate=100
 
     Parameters
     ----------
-    sample_numbers : np.array
-        Integer sample numbers for each clock line transition
+    timestamps_or_sample_numbers : np.array
+        Float times in seconds or integer sample numbers
+        for each clock line transition
     states : np.array
         States (1 or 0) for each clock line transition
     sample_rate : float
         The sample rate at which the clock signal was acquired
+        When inputting timestamps in seconds, use a sample rate of 1
     baud_rate : float
         The baud rate of the clock signal
 
     Returns
     -------
-    start_samples : np.array
-        Sample numbers at which each second begins
+    starts : np.array
+        Sample numbers or timestamps at which each second begins
     harp_times : np.array
         Harp clock times in seconds
     """
 
-    min_delta = int(sample_rate / 2)  # 0.5 seconds
+    min_delta = sample_rate / 2  # 0.5 seconds
 
-    barcode_edges = get_barcode_edges(sample_numbers, min_delta)
+    barcode_edges = get_barcode_edges(timestamps_or_sample_numbers, min_delta)
 
-    start_samples = [sample_numbers[edges[0]] for edges in barcode_edges]
+    start_samples = [timestamps_or_sample_numbers[edges[0]] for edges in barcode_edges]
 
     harp_times = [
         convert_barcode(
-            sample_numbers[edges[0] : edges[1]],
+            timestamps_or_sample_numbers[edges[0] : edges[1]] / sample_rate,
             states[edges[0] : edges[1]],
-            sample_rate=sample_rate,
             baud_rate=baud_rate,
         )
         for edges in barcode_edges
@@ -79,7 +82,8 @@ def get_barcode_edges(sample_numbers, min_delta):
     sample_numbers : np.array
         Sample numbers of clock line (high and low states)
     min_delta : int
-        The minimum number of samples between each barcode
+        The minimum length between the end of one
+        barcode and the start of the next
 
     Returns
     -------
@@ -92,18 +96,16 @@ def get_barcode_edges(sample_numbers, min_delta):
     return list(zip(splits[:-1] + 1, splits[1:] + 1))
 
 
-def convert_barcode(sample_numbers, states, sample_rate, baud_rate):
+def convert_barcode(transition_times, states, baud_rate):
     """
     Converts Harp clock barcode to a clock time in seconds
 
     Parameters
     ----------
-    sample_numbers : np.array
-        Integer sample numbers for each clock line transition
+    transition_times : np.array
+        Times (in seconds) each clock line transition
     states : np.array
         states (1 or 0) for each clock line transition
-    sample_rate : float
-        The sample rate at which the clock signal was acquired
     baud_rate : float
         The baud rate of the clock signal
 
@@ -114,25 +116,16 @@ def convert_barcode(sample_numbers, states, sample_rate, baud_rate):
 
     """
 
-    samples_per_bit = int(sample_rate / baud_rate)
-    middle_sample = int(samples_per_bit / 2)
-
-    intervals = np.diff(sample_numbers)
+    intervals = np.round(np.diff(transition_times * baud_rate)).astype("int")
 
     barcode = np.concatenate(
         [np.ones((count,)) * state for state, count in zip(states[:-1], intervals)]
     ).astype("int")
 
     val = np.concatenate(
-        [
-            np.arange(
-                samples_per_bit + middle_sample + samples_per_bit * 10 * i,
-                samples_per_bit * 10 * i - middle_sample + samples_per_bit * 10,
-                samples_per_bit,
-            )
-            for i in range(4)
-        ]
+        (np.arange(1, 9), np.arange(11, 19), np.arange(21, 29), np.arange(31, 39))
     )
+
     s = np.flip(barcode[val])
     harp_time = s.dot(2 ** np.arange(s.size)[::-1])
 
