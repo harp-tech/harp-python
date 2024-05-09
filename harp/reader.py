7662754a30cca373b724892234373783e5a49ab4
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-05-09 08:44:56 -0700

    Deprecate function

--- harp/reader.py
+++ harp/reader.py
@@ -340,7 +340,7 @@ def _create_register_handler(device: Model, name: str, params: _ReaderParams):
     reader = partial(reader, columns=columns)
     return RegisterReader(register, reader)
 
-
+@deprecated("This function is deprecated. Use DeviceReader.from_file, DeviceReader.from_url, DeviceReader.from_str, and DeviceReader.from_model instead.")
 def create_reader(
     device: Union[str, PathLike, Model],
     include_common_registers: bool = True,
