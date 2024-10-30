110250ea4cb520f209e98a83c241963bbed15548
glopesdev <glopesdev@users.noreply.github.com>
2024-10-30 12:08:22 +0000

    Apply ruff recommendations

--- harp/__init__.py
+++ harp/__init__.py
@@ -1,3 +1,5 @@
 from harp.io import REFERENCE_EPOCH, MessageType, read
 from harp.reader import create_reader
 from harp.schema import read_schema
+
+__all__ = ["REFERENCE_EPOCH", "MessageType", "read", "create_reader", "read_schema"]
