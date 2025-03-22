99665a62e0821e9a0b354000d4ba4de75f80165a
glopesdev <glopesdev@users.noreply.github.com>
2025-03-22 12:36:02 +0000

    Add conversion functions to base module

--- harp/__init__.py
+++ harp/__init__.py
@@ -1,5 +1,5 @@
-from harp.io import REFERENCE_EPOCH, MessageType, read
+from harp.io import REFERENCE_EPOCH, MessageType, read, to_buffer, to_file
 from harp.reader import create_reader
 from harp.schema import read_schema
 
-__all__ = ["REFERENCE_EPOCH", "MessageType", "read", "create_reader", "read_schema"]
+__all__ = ["REFERENCE_EPOCH", "MessageType", "read", "to_buffer", "to_file", "create_reader", "read_schema"]
